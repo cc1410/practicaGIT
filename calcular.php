99c9afd1e5c2a9f803a4f23584ff89c13d8f3b35
cc1410 <chen986755611@gmail.com>
2016-11-17 18:01:24 +0100

    subir datos.php

---
+++ calcular.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $numero[]=$_POST["numero1"];
+        $numero[]=$_POST["numero2"];
+        $numero[]=$_POST["numero3"];
+        $numero[]=$_POST["numero4"];
+        $numero[]=$_POST["numero5"];
+        $numero[]=$_POST["numero6"];
+
+        $sumar=0;
+        foreach ($numero as $suma) {
+            $sumar =$sumar+$suma;
+          
+        }  echo "la suma total es: $sumar<br>";
+        
+        $contar=count ($numero);
+        
+        
+        $medias=$sumar/$contar;
+        echo "la media total es: $medias<br>";
+        echo "<br>";
+        
+        $cont=0;
+        foreach ($numero as $par){
+            if($par%2==0){
+                $cont++;
+                
+                echo $par." es numero par<br>";
+            }
+        }echo "hay $cont numero pares<br>";
+        
+        
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="calcular.php" method="POST">
+            Numero1: <input type="number" name="numero1" value="numero1"><br>
+        Numero2: <input type="number" name="numero2" value="numero2"><br>
+        Numero3: <input type="number" name="numero3" value="numero3"><br>
+        Numero4: <input type="number" name="numero4" value="numero4"><br>
+        Numero5: <input type="number" name="numero5" value="numero5"><br>
+        Numero6: <input type="number" name="numero6" value="numero6"><br>
+        <input type="submit" name="enviar"><br>
+        </form>
+        
+    </body>
+</html>
